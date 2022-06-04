ab9e0d9503dc9fa1f85c7f2481092aaf6cf5a652
Peter <104941704+robertspwork@users.noreply.github.com>
2022-06-04 14:19:07 -0600

    Week 5 Commit
    In this code a logger and main method are used to push strings of my choice to the console.

---
+++ Week5CodingAssignment/src/week5Assignment2.java
@@ -0,0 +1,23 @@
+
+public class week5Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AsteriskLogger a = new AsteriskLogger ();
+		a.Log("Hypotheticals");
+		System.out.println();
+		a.Error("Very Litteral");
+		System.out.println();
+		
+		SpacedLogger s = new SpacedLogger();
+		s.Log("Hypotheticals");
+		System.out.println();
+		s.Error("Very Litteral");
+		System.out.println();
+		
+		
+		
+	}
+
+}
